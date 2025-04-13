e84189795b8ce436bd24f19eef95019c567485a2
Tyrone <28680107+ttaylor92@users.noreply.github.com>
2025-04-13 17:05:20 -0500

    Add Prettier for code formatting and enhance home page with search functionality
    - Added `prettier` as a dependency in `package.json` and `package-lock.json`.
    - Created `SearchForm` and `SearchFormReset` components for improved search functionality.
    - Updated the home page in `page.tsx` to include a search form and dynamic search results display.
    - Introduced a new configuration file for Prettier in `.idea/prettier.xml`.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const { query } = searchParams;
+
   return (
-    <div>
-      HOME
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+      </section>
+    </>
   );
 }
